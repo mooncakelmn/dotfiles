a72c15555901a571309076dce8f6395261ee42d5
mooncakelmn <allen-yb@163.com>
2021-05-06 14:31:22 +0800

    Update 2_robin_setup_shells.sh

--- 2_robin_setup_shells.sh
+++ 2_robin_setup_shells.sh
@@ -4,13 +4,14 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cp .aliases ~/.aliases
-cp .functions ~/.functions
-cp .exports ~/.exports
+# cp .functions ~/.functions
+# cp .exports ~/.exports
 cp .bash_profile ~/.bash_profile
 cp .zshrc  ~/.zshrc
 cp .gitconfig ~/.gitconfig
 
 # zsh plugin manager - antigen
-git clone git@github.com:zsh-users/antigen.git ~/code/zsh/antigen
+# git clone git@github.com:zsh-users/antigen.git ~/code/zsh/antigen
+gh repo clone zsh-users/antigen
 
 source ~/.zshrc
