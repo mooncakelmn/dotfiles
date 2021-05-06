67f9f3328db6a4b8f307ea544d34c282f2877f74
mooncakelmn <allen-yb@163.com>
2021-05-06 16:04:54 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -11,11 +11,11 @@ brew upgrade
 
 # GNU core utilities (those that come with OS X are outdated)
 brew install coreutils
-brew install moreutils
+# brew install moreutils
 # GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
-brew install findutils
+# brew install findutils
 # GNU `sed`, overwriting the built-in `sed`
-brew install gnu-sed --with-default-names
+# brew install gnu-sed --with-default-names
 
 
 # Bash 4
