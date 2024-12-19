524b282a042a3bd02cf8d45fc04b2aa079615804
mooncakelmn <allen-yb@163.com>
2024-12-19 17:57:55 +0800

    Update 1_robin_setup.sh

--- 1_robin_setup.sh
+++ 1_robin_setup.sh
@@ -8,6 +8,19 @@
   read -n 1
 
 # Homebrew
+# brew install --cask iterm2
+# brew install wget
+# brew install z
+# brew install eza
+# brew install git-delta
+# brew install yarn --ignore-dependencies # using node of nvm
+
+# alias, bash_profile, zshrc, gitconfig
+
+# NVM
+
+
+
 # install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -17,26 +30,4 @@
 
 # setup shells
 ./robin_setup_shells.sh
-
-# Node
-# install latest nvm
-cd ~/
-# git clone https://github.com/creationix/nvm.git .nvm
-gh repo clone nvm-sh/nvm .nvm
-cd ~/.nvm
-git checkout v0.38.0 # latest stable version
-
-nvm install lts # latest LTS version
-
-brew install yarn --ignore-dependencies # using node of nvm
-
-# rvm https://rvm.io/
-
-
-# Vim
-# vundle
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-gh repo clone VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
-cp ./.vimrc ~/.vimrc
-# Then open vim and run `:PluginInstall`
 
