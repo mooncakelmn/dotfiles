e667d598d8a67cfacca1aaa4be054b9a375e2059
mooncakelmn <allen-yb@163.com>
2024-12-27 13:51:56 +0800

    Update 1_robin_setup.sh

--- 1_robin_setup.sh
+++ 1_robin_setup.sh
@@ -13,12 +13,26 @@
 # brew install z
 # brew install eza
 # brew install git-delta
-# brew install yarn --ignore-dependencies # using node of nvm
+# brew install yarn --ignore-dependencies # using node of nvm, run `yarn config set disable-self-update-check true` after installation
 
 # alias, bash_profile, zshrc, gitconfig
 
 # NVM
 
+# VSCode
+# files to exlucde: node_modules,backstop_data/,web_portal/.next,web_portal/public,web_portal/.dist_dts,,coverage,.git,.nyc_output
+
+# mysql (Anaconda will install mysql 5.7, cool~ )
+# https://formulae.brew.sh/formula/mysql#default
+# Sequel Pro
+
+# python
+# Anaconda Navigator: https://www.anaconda.com/products/navigator 
+# Pycharm Community: https://www.jetbrains.com/pycharm/
+# copy xbx_stock_2019
+
+# Adobe Creative Cloud
+# https://account.adobe.com/products, login with Chrome save username/pwd
 
 
 # install
